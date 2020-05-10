213da56d9c9aa3ffdae47bc738621970ddc9ddad
Sebastian Wojdyga <wojdyga.sebastian@gmail.com>
2020-05-10 21:56:57 +0200

    Code Refactor in InjectClass (generic type)
    Replace generic type L (C>L<ass) into I (>I<nstance), because generic type is class instance, not class constructor.

--- src/resolvers/InjectClass/InjectClass.ts
+++ src/resolvers/InjectClass/InjectClass.ts
@@ -3,17 +3,17 @@ import InjectClassParams from './interfaces/InjectClassParams';
 import { Context } from '../../types/Context';
 import { Class } from "typescript-class-types";
 
-export default function InjectClass<L>(params: InjectClassParams<L>): Resolver {
+export default function InjectClass<I>(params: InjectClassParams<I>): Resolver {
     return {
-        injectClassHook<C extends Context, O extends {} | L>(context: C, constructor: Class<O>): Class<O> | void {
+        injectClassHook<C extends Context, O extends {} | I>(context: C, constructor: Class<O>): Class<O> | void {
             if(params.from === constructor) {
                 return params.to as Class<O>;
             }
         },
-        beforeCreateInstanceHook<C extends Context, O, A extends unknown[]>(context: C, constructor: Class<O>, constructorParams: A): O | void {
+        beforeCreateInstanceHook<C extends Context, O extends {} | I, A extends unknown[]>(context: C, constructor: Class<O>, constructorParams: A): O | void {
 
         },
-        afterCreateInstanceHook<C extends Context, O>(context: C, instance: O): void {
+        afterCreateInstanceHook<C extends Context, O extends {} | I>(context: C, instance: O): void {
 
         },
     }
