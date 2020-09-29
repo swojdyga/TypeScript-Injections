dc82a61bd2cd6d5fdffd93ccf3b62484cca1ff88
Sebastian Wojdyga <wojdyga.sebastian@gmail.com>
2020-09-29 21:36:32 +0200

    Adding support for abstract class in ContextType

--- src/resolversFactories/Contextual/factories/ContextType/ContextType.ts
+++ src/resolversFactories/Contextual/factories/ContextType/ContextType.ts
@@ -1,11 +1,11 @@
-import { Class } from 'typescript-class-types';
+import { AbstractClass } from 'typescript-class-types';
 import ContextualParamsContext from '../../interfaces/ContextualParamsContext';
 import { Context } from '../../../../types/Context';
 import { ResolvingElement } from '../../../../types/ResolvingElement';
 import IsConstructor from '../../../../helpers/IsConstructor/IsConstructor';
 import IsConstructorExtendsOf from '../../../../helpers/IsConstructorExtendsOf/IsConstructorExtendsOf';
 
-export default function ContextType(type: Class<object>): ContextualParamsContext {
+export default function ContextType(type: AbstractClass<object>): ContextualParamsContext {
     return {
         isInExpectedContext: (context: Context) => type instanceof Function && context instanceof type,
         isExpectedResolvingElement: (resolvingElement: ResolvingElement) => IsConstructor(resolvingElement) && IsConstructorExtendsOf(resolvingElement, type),
